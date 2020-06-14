7d8cb1330fe712d9e4330b905f671c7416f6234d
umbur <you@example.com>
2020-06-14 16:45:23 -0600

    passes prop to header

--- globomantics/src/main-page/header.js
+++ globomantics/src/main-page/header.js
@@ -6,7 +6,7 @@ const Header = (props) => (
     <div className="col-md-5">
       <img src={logo} className="logo" alt="logo" />
     </div>
-    <div className="col-md-7 mt-5 subtitle">Providing houses world wide</div>
+    <div className="col-md-7 mt-5 subtitle">{props.subtitle}</div>
   </header>
 );
 
--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -6,7 +6,7 @@ import Header from "./header";
 function App() {
   return (
     <div className="container">
-      <Header />
+      <Header subtitle="Providing houses all over the world" />
     </div>
   );
 }
