a6b989ab954c7a8cce82132cc8c5c229edcdc801
umbur <you@example.com>
2020-06-14 16:58:02 -0600

    adds fetch method to retrieve houses.json and creates a function to randomize the choice

--- globomantics/src/main-page/index.js
+++ globomantics/src/main-page/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./main-page.css";
 import Header from "./header";
 
-function App() {
-  return (
-    <div className="container">
-      <Header subtitle="Providing houses all over the world" />
-    </div>
-  );
+class App extends Component {
+  fetchHouses = () => {
+    fetch("./houses.json")
+      .then((rsp) => rsp.json())
+      .then((allHouses) => {
+        this.allHouses = allHouses;
+        this.determineFeaturedHouse();
+      });
+  };
+
+  determineFeaturedHouse = () => {
+    if (this.allHouses) {
+      const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+      const featuredHouse = this.allHouses[randomIndex];
+      this.setState({ featuredHouse });
+    }
+  };
+  render() {
+    return (
+      <div className="container">
+        <Header subtitle="Providing houses all over the world" />
+      </div>
+    );
+  }
 }
 
 export default App;
